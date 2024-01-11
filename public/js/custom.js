ae59629ed6410855b13ad035a6cf6e0b391e6390
kemalizzi213 <kemalizzi213@gmail.com>
2024-01-11 11:47:34 +0700

    Mengganti hero section dengan carousel

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,3 +7,21 @@ window.addEventListener("scroll", function () {
         navbar.classList.replace("nav-color", "bg-transparent");
     }
 });
+
+
+// Tanggal Check-in Check-out
+$(document).ready(function() {
+    $('#check-in').on('change', function() {
+        var checkInDate = $(this).val();
+        var nextDay = new Date(checkInDate);
+        nextDay.setDate(nextDay.getDate() + 1);
+        var nextDayFormatted = nextDay.toISOString().split('T')[0];
+        $('#check-out').prop('min', nextDayFormatted);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkIn = document.getElementById('check-in');
+    var today = new Date();
+    checkIn.setAttribute('min', today.toISOString().split('T')[0]);
+});
