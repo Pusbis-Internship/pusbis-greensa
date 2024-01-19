1ae8825a6ec5dd70650db1530adb72588bffdc36
zy1009 <muhammadramzy65@gmail.com>
2024-01-19 14:29:14 +0700

    admin dashbord kasaran

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,15 @@ class AdminController extends Controller
     public function showadmin()
     {return view('admin.index');}
 
+    public function showtrorder()
+    {return view('admin.trainorder');}
+
+    public function showtrlist()
+    {return view('admin.trainlist');}
+
+    public function showuser()
+    {return view('admin.userlist');}
+
     public function showadminold()
     {return view('admin.index_old');}
 }
