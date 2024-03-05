b7c1f26bb507f8532045bbc95ebffd94d51e9aad
kemalizzi213 <kemalizzi213@gmail.com>
2024-03-05 15:48:07 +0700

    feat
    repair layout payment

--- resources/views/pelanggan/page/payment.blade.php
+++ resources/views/pelanggan/page/payment.blade.php
@@ -1,48 +1,50 @@
-@extends('pelanggan.layout.index')
+@extends('pelanggan.layout.loginRegis')
 
 @section('content')
-    <section class="breadcrumb-section section-b-space" style="padding-top: 100px;padding-bottom:100px;">
-        <ul class="circles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="text-white">Order Payment</h1>
-                    <nav>
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item">
-                                <a href="../index.htm">
-                                    <i class="fas fa-home text-white"></i>
-                                </a>
-                            </li>
-                            <li class="breadcrumb-item active text-white" aria-current="page">Order Payment</li>
-                        </ol>
-                    </nav>
+
+<div class="container my-5">
+        <div class="row header-table">
+            <div class="col-12 p-0">
+                <form action="/order">
+                    <button class="btn btn-success mb-3">Kembali ke halaman order</button>
+                </form>
+            </div>
+
+            {{-- BSI --}}
+            <div class="col-12 card d-flex align-items-center justify-content-center text-center">
+                <p class="m-0 mt-5">Segera transfer sejumlah <span class="fw-bold text-success">Rp. 3.000.000</span> ke
+                    rekening berikut :</p>
+                <img src="{{ asset('assets/images/BSI.png') }}" alt="" class="mt-3" style="width: 30%">
+                <h5 class="m-0 mt-4">030-098-0976</h5>
+                <p class="text-secondary">a/n Greensa</p>
+                <h1 class="mb-3 display-2 fw-bold text-success">01:00:00</h1>
+
+                <div class="col-5 mb-3">
+                    <label class="labels">Upload bukti transfer<span class="text-danger">*</span></label>
+                    <input type="file" name="surat" accept=".pdf" class="form-control" placeholder="Upload file SPJ" required>
+                    <div class="col-12">
+                        <button class="btn btn-success my-4 w-100">Kirim</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
 
-    <form action="/order">
-        <button class="btn btn-success">Kembali ke halaman order</button>
-    </form>
+            {{-- BTN --}}
+            <div class="col-12 card d-flex align-items-center justify-content-center text-center">
+                <p class="m-0 mt-5">Segera transfer sejumlah <span class="fw-bold text-success">Rp. 3.000.000</span> ke
+                    rekening berikut :</p>
+                <img src="{{ asset('assets/images/BTN.png') }}" alt="" class="mt-5" style="width: 30%">
+                <h5 class="m-0 mt-4">030-098-0976</h5>
+                <p class="text-secondary">a/n Greensa</p>
+                <h1 class="mb-3 display-2 fw-bold text-success">01:00:00</h1>
 
-    <div class="container my-5" >
-        <div class="row header-table border-top border-bottom d-md-flex d-none align-items-center py-3">
-            <h1></h1>
-            <h1>ini Countdown waktu 1 jam</h1>
-            <h1></h1>
-            <button class="btn btn-success">Saya sudah bayar</button>
+                <div class="col-5 mb-3">
+                    <label class="labels">Upload bukti transfer<span class="text-danger">*</span></label>
+                    <input type="file" name="surat" accept=".pdf" class="form-control" placeholder="Upload file SPJ" required>
+                    <div class="col-12">
+                        <button class="btn btn-success my-4 w-100">Kirim</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
